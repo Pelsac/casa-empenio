2c3939f4df9d46df43a312f33e4cddd50fc5cc1f
Pedro Luis S.C <sanchezplayc@gmail.com>
2021-09-04 11:03:23 -0500

    Se modifico la base de datos datos

--- BD/casa-empenio.sql
+++ BD/casa-empenio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-08-2021 a las 18:01:15
+-- Tiempo de generación: 04-09-2021 a las 17:54:33
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cliente` (
   `cedula` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
+  `nombreC` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `celular` bigint(20) DEFAULT NULL,
   `correo` varchar(150) DEFAULT NULL
@@ -43,10 +43,12 @@ CREATE TABLE `cliente` (
 --
 
 CREATE TABLE `compra` (
-  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cedula_cliente` int(11) NOT NULL,
   `total_productos` int(11) NOT NULL,
   `total_pagar` decimal(10,0) NOT NULL,
-  `cedula_empleado` int(11) NOT NULL
+  `fecha` datetime NOT NULL,
+  `detalle` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,10 +75,22 @@ CREATE TABLE `empleado` (
 
 CREATE TABLE `estanteria` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
   `capacidad_filas` int(11) DEFAULT NULL,
-  `capacidad_columnas` int(11) DEFAULT NULL
+  `capacidad_columnas` int(11) DEFAULT NULL,
+  `filas_ocupadas` int(11) NOT NULL,
+  `columnas_ocupadas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estanteria`
+--
+
+INSERT INTO `estanteria` (`id`, `nombre`, `capacidad_filas`, `capacidad_columnas`, `filas_ocupadas`, `columnas_ocupadas`) VALUES
+(1, 'Tecnologia ', 200, 200, 0, 0),
+(2, 'Joyas', 200, 200, 0, 0),
+(3, 'Ventas', 100, 100, 0, 0),
+(4, 'Electrodoméstico', 200, 200, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -86,10 +100,9 @@ CREATE TABLE `estanteria` (
 
 CREATE TABLE `pago_empenio` (
   `id` int(11) NOT NULL,
-  `valor` decimal(10,0) NOT NULL,
+  `valor_pagado` decimal(10,0) NOT NULL,
   `fecha` datetime DEFAULT NULL,
-  `cedula_cliente` int(11) DEFAULT NULL,
-  `cedula_empleado` int(11) DEFAULT NULL
+  `cedula_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -100,42 +113,18 @@ CREATE TABLE `pago_empenio` (
 
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
   `valor_empenio` decimal(10,0) NOT NULL,
   `precio_venta` decimal(10,0) DEFAULT NULL,
   `estado` varchar(50) NOT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
   `fecha_inicial` date NOT NULL,
   `fecha_final` date NOT NULL,
-  `ubicacion` varchar(50) NOT NULL,
+  `ubicacion_fila` int(11) NOT NULL,
+  `ubicacion_columna` int(11) NOT NULL,
   `cedula_cliente` int(11) NOT NULL,
   `id_estanteria` int(11) NOT NULL,
-  `id_compra` int(11) DEFAULT NULL,
   `cedula_empleado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `realiza_compra`
---
-
-CREATE TABLE `realiza_compra` (
-  `cedula_cliente` int(11) NOT NULL,
-  `id_compra` int(11) NOT NULL,
-  `fecha_compra` datetime DEFAULT NULL,
-  `detalle_compra` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `requiere`
---
-
-CREATE TABLE `requiere` (
-  `id_compra` int(11) NOT NULL,
-  `id_producto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -147,7 +136,7 @@ CREATE TABLE `requiere` (
 CREATE TABLE `usuario` (
   `username` varchar(150) NOT NULL,
   `password` varchar(150) NOT NULL,
-  `ultima_sesion` datetime NOT NULL,
+  `ultima_sesion` datetime DEFAULT NULL,
   `cedula_empleado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -165,8 +154,7 @@ ALTER TABLE `cliente`
 -- Indices de la tabla `compra`
 --
 ALTER TABLE `compra`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cedula_empleado` (`cedula_empleado`);
+  ADD PRIMARY KEY (`cedula_cliente`,`id_producto`);
 
 --
 -- Indices de la tabla `empleado`
@@ -185,8 +173,7 @@ ALTER TABLE `estanteria`
 --
 ALTER TABLE `pago_empenio`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `cedula_cliente` (`cedula_cliente`),
-  ADD KEY `cedula_empleado` (`cedula_empleado`);
+  ADD KEY `cedula_cliente` (`cedula_cliente`);
 
 --
 -- Indices de la tabla `producto`
@@ -195,20 +182,7 @@ ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cedula_cliente` (`cedula_cliente`),
   ADD KEY `id_estanteria` (`id_estanteria`),
-  ADD KEY `id_compra` (`id_compra`),
   ADD KEY `cedula_empleado` (`cedula_empleado`);
-
---
--- Indices de la tabla `realiza_compra`
---
-ALTER TABLE `realiza_compra`
-  ADD PRIMARY KEY (`cedula_cliente`,`id_compra`);
-
---
--- Indices de la tabla `requiere`
---
-ALTER TABLE `requiere`
-  ADD PRIMARY KEY (`id_compra`,`id_producto`);
 
 --
 -- Indices de la tabla `usuario`
@@ -218,21 +192,30 @@ ALTER TABLE `usuario`
   ADD KEY `cedula_empleado` (`cedula_empleado`);
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `compra`
+-- AUTO_INCREMENT de la tabla `pago_empenio`
 --
-ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`cedula_empleado`) REFERENCES `empleado` (`cedula`);
+ALTER TABLE `pago_empenio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restricciones para tablas volcadas
+--
 
 --
 -- Filtros para la tabla `pago_empenio`
 --
 ALTER TABLE `pago_empenio`
-  ADD CONSTRAINT `pago_empenio_ibfk_1` FOREIGN KEY (`cedula_cliente`) REFERENCES `cliente` (`cedula`),
-  ADD CONSTRAINT `pago_empenio_ibfk_2` FOREIGN KEY (`cedula_empleado`) REFERENCES `empleado` (`cedula`);
+  ADD CONSTRAINT `pago_empenio_ibfk_1` FOREIGN KEY (`cedula_cliente`) REFERENCES `cliente` (`cedula`);
 
 --
 -- Filtros para la tabla `producto`
@@ -240,8 +223,7 @@ ALTER TABLE `pago_empenio`
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`cedula_cliente`) REFERENCES `cliente` (`cedula`),
   ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`id_estanteria`) REFERENCES `estanteria` (`id`),
-  ADD CONSTRAINT `producto_ibfk_3` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id`),
-  ADD CONSTRAINT `producto_ibfk_4` FOREIGN KEY (`cedula_empleado`) REFERENCES `empleado` (`cedula`);
+  ADD CONSTRAINT `producto_ibfk_3` FOREIGN KEY (`cedula_empleado`) REFERENCES `empleado` (`cedula`);
 
 --
 -- Filtros para la tabla `usuario`
